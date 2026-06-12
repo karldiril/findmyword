828d53cc123ad4350eeb4984d2465039d014b03f
Karl <karld@MacBook-Air-de-Karl.local>
2026-06-12 10:14:10 +0200

    feat: add Word class

---
+++ TestWord.java
@@ -0,0 +1,7 @@
+public class TestWord {
+    public static void main(String[] args) {
+
+        Word mot = new Word("point");
+        System.out.println(mot.valide());
+    }
+}
---
+++ Word.java
@@ -0,0 +1,41 @@
+public class Word {
+    public static final int TAILLE_MOT = 5;
+
+    private String texte;
+
+    public Word(String texte) {
+        this.texte = texte;
+    }
+
+    public String getTexte() {
+        return this.texte;
+    }
+
+    public boolean valide() {
+        if (this.texte.length() != TAILLE_MOT) {
+            return false;
+        }
+
+        for (int i = 0; i < this.texte.length(); i++) {
+            char c = this.texte.charAt(i);
+
+            if (!Character.isLetter(c)) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < this.texte.length(); i++) {
+            for (int j = i + 1; j < this.texte.length(); j++) {
+                if (this.texte.charAt(i) == this.texte.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public String toString() {
+        return this.texte;
+    }
+}
