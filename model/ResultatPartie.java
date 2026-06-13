3f393fe7f623edf37875b9d3622dfc6e49cda35a
Karl0259 <karldiril6@gmail.com>
2026-06-13 11:47:01 +0200

    chore: add constants for game modes in ResultatPartie

--- model/ResultatPartie.java
+++ model/ResultatPartie.java
@@ -1,6 +1,10 @@
 package model;
 
 public class ResultatPartie {
+
+    public static final String MODE_POINT = "POINT";
+    public static final String MODE_CHRONO = "CHRONO";
+
     private String modeJeu;
     private String motSecret;
     private boolean trouve;
