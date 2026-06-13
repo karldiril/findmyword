6b64cda136320384c8e328bf00cf28da29db2ec7
Karl0259 <karldiril6@gmail.com>
2026-06-13 11:14:00 +0200

    feat: implement GameUI class

---
+++ view/GameUI.java
@@ -0,0 +1,83 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import model.Word;
+import model.Joueur;
+
+
+public class GameUI {
+    private Scanner scanner;
+
+    public GameUI() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void afficherAccueil() {
+        System.out.println("=====================================");
+        System.out.println("       FIND MY WORD - BUT1           ");
+        System.out.println("      Créé par Iyad et Karl          ");
+        System.out.println("=====================================");
+        System.out.println();
+    }
+
+    public String demanderNomJoueur(int numeroJoueur) {
+        System.out.print("Entrez le nom du joueur " + numeroJoueur + " : ");
+        return scanner.nextLine();
+    }
+
+    public String demanderSaisieMot(String nomJoueur) {
+        System.out.print("\n" + nomJoueur + ", proposez un mot : ");
+        return scanner.nextLine().toUpperCase();
+    }
+
+    public void afficherErreurValidation(int codeErreur) {
+        if (codeErreur == Word.ERREUR_LONGUEUR) {
+            System.out.println("Erreur : le mot doit faire " + Word.TAILLE_MOT + "lettres.");
+        }
+        else if (codeErreur == Word.ERREUR_CARACTERE) {
+            System.out.println("Erreur : Seules les lettres sont autorisées.");
+        }
+        else if (codeErreur == Word.ERREUR_DOUBLON) {
+            System.out.println("Erreur : Pas de lettres en double dans le mot.");
+        }
+    }
+
+    public void afficherGrille(ArrayList<Word> tentatives, ArrayList<String[]> analyses) {
+        System.out.println("\n--- Etat de la partie ---");
+        for (int i = 0; i < tentatives.size(); i++) {
+            String mot = tentatives.get(i).getTexte().toUpperCase();
+            String[] analyse = analyses.get(i);
+
+            for (int j = 0; j < mot.length(); j++) {
+                System.out.print("[ " + mot.charAt(j) + " ]");
+            }
+
+            System.out.print(" --> ");
+
+            for (String etat : analyse) {
+                System.out.printf("%-10s", etat);
+            }
+            System.out.println();
+        }
+    }
+
+    public void afficherFinPartie(boolean gagne, Word motSecret) {
+        System.out.println("\n--------------------------------");
+        if (gagne) {
+            System.out.println("Bravo !! Le mot était bien : " + motSecret.getTexte().toUpperCase() + " !!!");
+        }
+        else {
+            System.out.println("La partie est terminé et vous n'avez pas trouvé le mot :'(");
+        }
+        System.out.println("-------------------------------------------");
+    }
+
+
+    public void afficherResultatsFinaux(ArrayList<Joueur> joueurs) {
+        System.out.println("\n==== Resultats Finaux ===");
+        for (Joueur j : joueurs) {
+            System.out.println("Joueur : " + j.getNom() + " | Score : " + j.getScoreGlobal());
+        }
+    }
+}
