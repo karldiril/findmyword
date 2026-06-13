9e0f9b993d2d1d229c8b6c2eebd23027aa1725c9
Karl0259 <karldiril6@gmail.com>
2026-06-13 07:48:25 +0200

    feat: add Game abstract controller

---
+++ controller/Game.java
@@ -0,0 +1,99 @@
+package controller;
+
+import java.util.ArrayList;
+import model.Joueur;
+import model.Word;
+import repository.WordRepository;
+
+
+public abstract class Game {
+    private Word motSecret;
+    private WordRepository repository;
+    private ArrayList<Word> tentatives = new ArrayList<>();
+    private ArrayList<String[]> analyses = new ArrayList<>();
+    private ArrayList<Joueur> joueurs;
+    private int maxEssais = 6;
+    private int indexJoueurActuel = 0;
+    private boolean partieTerminee = false;
+
+
+    public Game(WordRepository repo, ArrayList<Joueur> listeJoueurs) {
+        this.repository = repo;
+        this.joueurs = listeJoueurs;
+        this.motSecret = this.repository.getWord();
+    }
+
+
+    public Word getMotSecret() {
+        return this.motSecret;
+    }
+
+    public ArrayList<Word> getTentatives() {
+        return this.tentatives;
+    }
+
+    public ArrayList<String[]> getAnalyses() {
+        return this.analyses;
+    }
+
+    public ArrayList<Joueur> getJoueurs() {
+        return this.joueurs;
+    }
+
+    public int getMaxEssais() {
+        return this.maxEssais;
+    }
+
+    public int getIndexJoueurActuel() {
+        return this.indexJoueurActuel;
+    }
+
+    public boolean isPartieTerminee() {
+        return this.partieTerminee;
+    }
+
+    public void setPartieTerminee(boolean etat) {
+        this.partieTerminee = etat;
+    }
+
+    public void changerJoueur() {
+        if (this.joueurs != null && !this.joueurs.isEmpty()) {
+            this.indexJoueurActuel = (this.indexJoueurActuel + 1) % this.joueurs.size();
+        }
+    }
+
+    public String[] analyserTentatives(Word proposition) {
+        String[] resultat = new String[Word.TAILLE_MOT];
+        String propositionTexte = proposition.getTexte();
+        String motSecretTexte = this.motSecret.getTexte();
+
+        for (int i = 0; i < propositionTexte.length(); i++) {
+            if (estLettreBienPlacee(i, proposition))
+                resultat[i] = "OK";
+            else if (compterOccurence(propositionTexte.charAt(i), motSecretTexte) > 0)
+                resultat[i] = "PRESENT";
+            else
+                resultat[i] = "ABSENT";
+        }
+        this.tentatives.add(proposition);
+        this.analyses.add(resultat);
+        return resultat;
+    }
+
+    private boolean estLettreBienPlacee(int index, Word proposition) {
+        return proposition.getTexte().charAt(index) == this.motSecret.getTexte().charAt(index);
+    }
+
+    private int compterOccurence(char lettre, String texte) {
+        int compteur = 0;
+        for (int i = 0; i < texte.length(); i++) {
+            if (texte.charAt(i) == lettre) {
+                compteur++;
+            }
+        }
+        return compteur;
+    }
+
+    public abstract void executerActionAvantSaisie();
+    public abstract void verifierFinDeTour(int numEssai, boolean aTrouve);
+}
